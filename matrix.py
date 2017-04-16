34dc55068f5d2689ef8539d8f672b989cc9fb8bf
Arseniy Trushin <142857@mail.ru>
2017-04-16 18:02:35 +0300

    tests for matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+
+import unittest
+
 class Matrix(object):
     def __init__(self, data):
         if type(data) == list and type(data[0]) == list:
@@ -8,3 +12,19 @@ class Matrix(object):
 
     def set(self, p, q, val):
         self.dat[p][q] = val
+
+    def height(self):
+        return len(self.dat)
+
+    def width(self):
+        return len(self.dat[0])
+
+class TestMatrixConstruction(unittest.TestCase):
+    def test_create_by_matrix(self):
+        m = Matrix([[11, 12], [21, 22], [31, 32]])
+        self.assertEqual(m.width(), 2)
+        self.assertEqual(m.height(), 3)
+        self.assertEqual(m.get(0, 0), 11)
+
+if __name__ == '__main__':
+    unittest.main()
