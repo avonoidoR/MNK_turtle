aaa32cd480c387295811d6a06992a464b1ec528a
Arseniy Trushin <142857@mail.ru>
2017-04-20 16:28:27 +0300

    star

---
+++ star.py
@@ -0,0 +1,11 @@
+import sys as s
+for h in range(6):
+    if h%2==0:
+        n='*'
+    else:
+        n=' '
+    for x in range(5):
+        s.stdout.write(n)
+        s.stdout.write('\n')
+        
+            
